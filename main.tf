4191d09be9a54b95712b0c54611d18179f16ab3e
terraformjpoole <97690124+terraformjpoole@users.noreply.github.com>
2022-01-13 11:02:40 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    organization = "hashicorp-learn"
+    organization = "jefflance10_test_terraform"
 
     workspaces {
       name = "learn-terraform-pipelines-vault"
